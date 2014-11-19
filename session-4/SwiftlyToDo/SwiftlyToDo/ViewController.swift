487512bfd34570f5eaec705a472cf73af958d52a
Ciaran O hUallachain <ciaran@Spartacus.local>
2014-11-19 20:17:04 +0000

    added delete functionality

--- session-4/SwiftlyToDo/SwiftlyToDo/ViewController.swift
+++ session-4/SwiftlyToDo/SwiftlyToDo/ViewController.swift
@@ -11,9 +11,11 @@ import CoreData
 
 class ViewController: UITableViewController, ToDoViewControllerDelegate {
     var toDoItems: [ToDoEntity] = []
+    @IBOutlet var editButton: UIBarButtonItem! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loadToDoData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -52,6 +54,7 @@ class ViewController: UITableViewController, ToDoViewControllerDelegate {
         var success = context.save(&error)
         
         if (success) {
+            self.loadToDoData()
             self.tableView.reloadData()
             self.navigationController?.popViewControllerAnimated(true)
         }
@@ -69,6 +72,53 @@ class ViewController: UITableViewController, ToDoViewControllerDelegate {
             toDoController.toDoItem = toDoItem
         }
         toDoController.toDoSaveDelegate = self
+    }
+    
+    @IBAction func editTable(sender: UIBarButtonItem) {
+        self.setEditing(!self.tableView.editing, animated: true)
+    }
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        self.editButton.title = editing ? "Done" : "Edit"
+        super.setEditing(editing, animated: animated)
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            let toDoItem = self.toDoItems[indexPath.row]
+            let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+            let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+            context.deleteObject(toDoItem)
+            
+            var error: NSError? = nil
+            var success = context.save(&error)
+            
+            if (success) {
+                self.loadToDoData()
+                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
+            }
+        }
+    }
+    
+    func loadToDoData() {
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+        var error: NSError? = nil
+        var fetchRequest: NSFetchRequest = NSFetchRequest(entityName: "ToDoEntity")
+        fetchRequest.resultType = NSFetchRequestResultType.ManagedObjectResultType
+        let results: NSArray = context.executeFetchRequest(fetchRequest, error:  &error)!
+        
+        self.toDoItems = [ToDoEntity]()
+        for data in results {
+            var toDoEntity = data as ToDoEntity
+            self.toDoItems.append(toDoEntity)
+        }
     }
 }
 
